14b7f04d614241b62bff0b1f81b63b06d752e4a0
Ugolovka <Medusa_Gangrena@tutamail.com>
2025-05-19 13:09:37 +0300

    ydtydfty

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from processing import DataProcessor
 
 if __name__ == "__main__":
-    input_file = 'var9.csv'
+    input_file = 'var9 1.csv'
     processor = DataProcessor(input_file)    
     processor.read_dataframe()
+    
 
     
--- processing.py
+++ processing.py
@@ -5,12 +5,19 @@ import pandas as pd
 class DataProcessor:
     def __init__(self, filename):
         self.filename = filename
-        self.expected_columns = ['Участники гражданского оборота','Тип операции','Сумма операции','Вид расчета','Место оплаты','Терминал оплаты','Дата оплаты','Время оплаты','Результат операции','Cash-back','Сумма cash-back']
+        self.expected_columns = ['Участники гражданского оборота', 'Тип операции', 'Сумма операции', 'Вид расчета', 'Место оплаты', 'Терминал оплаты', 'Дата оплаты', 'Время оплаты', 'Результат операции', 'Cash-back', 'Сумма cash-back']
         self.expected_dtypes = {
-            'A': int,
-            'B': str,
-            'C': float,
-            'D': float
+            'Участники гражданского оборота': object,
+            'Тип операции': object,
+            'Сумма операции': float,
+            'Вид расчета': object,
+            'Место оплаты': object,
+            'Терминал оплаты': object,
+            'Дата оплаты': object,
+            'Время оплаты': object,
+            'Результат операции': object,
+            'Cash-back':object,
+            'Сумма cash-back':float
         }
 
     def read_dataframe(self):
@@ -47,10 +54,15 @@ class DataProcessor:
 
         for col, expected_type in self.expected_dtypes.items():
             if col in df.columns:
-                actual_type = df[col].dropna().map(type).mode()[0] if not df[col].dropna().empty else None
-                if actual_type != expected_type:
+                actual_type = df[col].dropna().dtype
+
+                expected_numpy_type = pd.Series(dtype=expected_type).dtype
+
+                if actual_type != expected_numpy_type:
                     errors.append(f"В столбце '{col}' тип данных не соответствует ожидаемому.")
-                    errors.append(f"Ожидается: {expected_type.__name__}, фактически: {actual_type.__name__ if actual_type else 'пусто'}")
+                    errors.append(f"Ожидается: {expected_numpy_type}, фактически: {actual_type}")
+
+
 
         if errors:
             print("Структура датафрейма НЕ соответствует ожидаемой:")
